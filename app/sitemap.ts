95fb56f63a8b7aa9480e3b66fac826b8238e7104
lalit nath <lalitwebd@bigwigmediadigital.com>
2026-09-22 16:11:39 +0530

    sitemap url update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://a7sattaking.co";
+  const baseUrl = "https://www.a7sattaking.co";
 
   return [
     {
